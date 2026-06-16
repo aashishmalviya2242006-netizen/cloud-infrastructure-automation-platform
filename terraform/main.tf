68415000fd3df311f8a1d8328f11a27edfe237fe
Aashish Malviya <aashishmalviya2242006@gmail.com>
2026-06-16 07:11:34 +0000

    terrfaorm: add public subnet resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,4 +5,11 @@ resource "aws_vpc" "main" {
      Name = "devsecops-project-vpc"
 }
 }
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 
+  tags = {
+    Name = "public-subnet"
+  }
+}
