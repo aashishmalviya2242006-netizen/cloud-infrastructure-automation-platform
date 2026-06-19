328d12c9c56cf5711f5910c1b545560da146bd09
Aashish Malviya <aashishmalviya2242006@gmail.com>
2026-06-19 06:05:46 +0000

    terraform: add ec2 web server resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,3 +81,17 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+
+resource "aws_instance" "web_server" {
+  ami                    = "ami-006f82a1d5a27da54"
+  instance_type          = "t3.micro"
+  key_name               = "terraform-key"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "terraform-web-server"
+  }
+}
