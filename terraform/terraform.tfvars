75770978686022bb43caada51a5dcd0ce8ee958f
Aashish Malviya <aashishmalviya2242006@gmail.com>
2026-06-20 07:51:45 +0000

    terraform: move variable values to tfvars

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+vpc_cidr            = "10.0.0.0/16"
+subnet_cidr         = "10.0.1.0/24"
+private_subnet_cidr = "10.0.2.0/24"
+
+instance_type = "t3.micro"
+key_name      = "terraform-key"
+
+ami_id = "ami-006f82a1d5a27da54"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,11 @@
-variable "vpc_cidr" {
-   default = "10.0.0.0/16"
-}
+variable "vpc_cidr" {}
 
-variable "subnet_cidr" {
-  default = "10.0.1.0/24"
-}
+variable "subnet_cidr" {}
 
-variable "instance_type" {
-  default = "t3.micro"
-}
+variable "private_subnet_cidr" {}
 
-variable "key_name" {
-  default = "terraform-key"
-}
+variable "instance_type" {}
 
-variable "ami_id" {
-  default = "ami-006f82a1d5a27da54"
-}
+variable "key_name" {}
 
-variable "private_subnet_cidr" {
-  default = "10.0.2.0/24"
-}
-
-
+variable "ami_id" {}
